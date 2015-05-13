d1e5ce8c5204aaa5ce2c0b1d823d3cd5e821427b
abdul18 <abdul18rahman@gmail.com>
2015-05-13 11:04:32 +0530

    before do json parse receiveing json hide and check post data

--- ship_app.rb
+++ ship_app.rb
@@ -5,9 +5,9 @@ require 'active_support/core_ext/hash/indifferent_access'
 class ShipApp < Sinatra::Base
   attr_reader :payload
 
-  before do
-    @payload = JSON.parse(request.body.read).with_indifferent_access
-  end
+  # before do
+  #   @payload = JSON.parse(request.body.read).with_indifferent_access
+  # end
 
   post "/get_shipments" do
     content_type :json
