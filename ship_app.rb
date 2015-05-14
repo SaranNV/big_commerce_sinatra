17967fd5abb89273c302a1217bcef722bfdfc0c4
abdul18 <abdul18rahman@gmail.com>
2015-05-14 12:45:40 +0530

    post api call test

--- ship_app.rb
+++ ship_app.rb
@@ -23,8 +23,8 @@ class ShipApp < Sinatra::Base
             body: json_payload,
             headers: {
                 'Content-Type'       => 'application/json',
-                # 'X-Hub-Store'        => '5551e429736d6164084f0000',
-                # 'X-Hub-Access-Token' => 'ef72138b58869394a224dad3ce90d4e5ae677d4eaaa6a891',
+                'X-Hub-Store'        => '5551e429736d6164084f0000',
+                'X-Hub-Access-Token' => 'ef72138b58869394a224dad3ce90d4e5ae677d4eaaa6a891',
                 'X-Hub-Timestamp'    => Time.now.utc.to_i.to_s
             }
         }
