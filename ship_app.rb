5a1673474ecac1684c86fb6bef5576620074db05
abdul18 <abdul18rahman@gmail.com>
2015-05-18 17:08:03 +0530

    check push the data

--- ship_app.rb
+++ ship_app.rb
@@ -15,13 +15,37 @@ class ShipApp < Sinatra::Base
 
   post '/test_shipment' do
 
-    payload = @payload
+    payload = "{\"request_id\":\"52f367367575e449c3000001\",\"shipment\":{\"id\":\"111\",\"order_id\":\"R154085346\",\"email\":\"spree@example.com\",\"cost\":5,\"status\":\"shipped\",\"stock_location\":\"default\",\"shipping_method\":\"UPS Ground (USD)\",\"tracking\":\"12345678\",\"updated_at\":null,\"shipped_at\":\"2014-02-03T17:33:55.343Z\",\"shipping_address\":{\"firstname\":\"Joe\",\"lastname\":\"Smith\",\"address1\":\"1234 Awesome Street\",\"address2\":\"\",\"zipcode\":\"90210\",\"city\":\"Hollywood\",\"state\":\"California\",\"country\":\"US\",\"phone\":\"0000000000\"},\"items\":[{\"name\":\"xxx\",\"sku\":\"XXX\",\"external_ref\":\"\",\"quantity\":1,\"price\":30,\"variant_id\":73,\"options\":{}}]}}"
     options = {
-        headers: { "Content-Type" => "application/json", "X-Hub-Store" => "5551e429736d6164084f0000", "X-Hub-Access-Token" => "ef72138b58869394a224dad3ce90d4e5ae677d4eaaa6a891", "X-Hub-Timestamp" => Time.now.utc.to_i.to_s},
-        parameters: payload.to_json
+        headers: {"Content-Type" => "application/json", "X-Hub-Store" => "5551e429736d6164084f0000", "X-Hub-Access-Token" => "ef72138b58869394a224dad3ce90d4e5ae677d4eaaa6a891", "X-Hub-Timestamp" => Time.now.utc.to_i.to_s},
+        parameters: {
+            "shipment" => {
+                "id" => "1283645",
+                "order_id" => "R154085346",
+                "email" => "spree@example.com",
+                "cost" => 5,
+                "status" => "ready",
+                "stock_location" => "default",
+                "shipping_method" => "UPS Ground (USD)",
+                "tracking" => "12345678",
+                "shipped_at" => "2014-02-03T17:33:55.343Z",
+                "channel" => "spree",
+                "totals" => {
+                    "item" => 200,
+                    "adjustment" => 10,
+                    "tax" => 10,
+                    "shipping" => 0,
+                    "payment" => 210,
+                    "order" => 210
+                }
+            }
+        }
+
     }
 
-    response = Service.request :post,  options
+    puts options.to_json
+
+    response = Service.request :post, options
     result 200, "Shipment transmitted to ShipStation: #{response.body["orderId"]}"
   end
 
@@ -30,7 +54,7 @@ class ShipApp < Sinatra::Base
     request_id = payload[:request_id]
 
     shipments = Service.new(payload).shipments_since
-    { request_id: request_id, shipments: shipments }.to_json
+    {request_id: request_id, shipments: shipments}.to_json
   end
 
   post "/add_shipment" do
@@ -38,7 +62,7 @@ class ShipApp < Sinatra::Base
     request_id = payload[:request_id]
 
     shipment = Service.new(payload).create
-    { request_id: request_id, summary: "Shipment #{shipment} was added" }.to_json
+    {request_id: request_id, summary: "Shipment #{shipment} was added"}.to_json
   end
 
   post "/update_shipment" do
@@ -46,15 +70,15 @@ class ShipApp < Sinatra::Base
     request_id = payload[:request_id]
 
     shipment = Service.new(payload).update
-    { request_id: request_id, summary: "Shipment #{shipment} was updated" }.to_json
+    {request_id: request_id, summary: "Shipment #{shipment} was updated"}.to_json
   end
-  
+
   post "/get_picked_up" do
     content_type :json
     request_id = payload[:request_id]
 
     shipments = Service.new(payload).picked_up
-    { request_id: request_id, shipments: shipments }.to_json
+    {request_id: request_id, shipments: shipments}.to_json
   end
 
   # Custom webhook
@@ -63,9 +87,9 @@ class ShipApp < Sinatra::Base
     request_id = payload[:request_id]
 
     shipment = Service.new(payload).cancel
-    { request_id: request_id, summary: "Shipment #{shipment} was canceled" }.to_json
+    {request_id: request_id, summary: "Shipment #{shipment} was canceled"}.to_json
   end
-  end
+end
 
 
 class Service
@@ -79,11 +103,11 @@ class Service
   # Search for shipments after a given timestamp, e.g. payload[:created_after]
   def shipments_since
     [
-      {
-        "id" => "12836",
-        "status" => "shipped",
-        "tracking" => "12345678"
-      }
+        {
+            "id" => "12836",
+            "status" => "shipped",
+            "tracking" => "12345678"
+        }
     ]
   end
 
@@ -91,8 +115,8 @@ class Service
   #   FedEx.get_picked_up payload
   def picked_up
     [
-      { "id" => "12836", "status" => "picked_up", "picked_up_at" => "2014-02-03T17:29:15.219Z" },
-      { "id" => "13243", "status" => "picked_up", "picked_up_at" => "2014-02-03T17:03:15.219Z" }
+        {"id" => "12836", "status" => "picked_up", "picked_up_at" => "2014-02-03T17:29:15.219Z"},
+        {"id" => "13243", "status" => "picked_up", "picked_up_at" => "2014-02-03T17:03:15.219Z"}
     ]
   end
 
@@ -116,15 +140,42 @@ class Service
 
 
   def self.request(method, options)
-     base_uri = "https://push.wombat.co"
-    response = HTTParty.post("https://push.wombat.co", options)
+    base_uri = "https://push.wombat.co"
+    # response = HTTParty.post("https://push.wombat.co", options)
+
+    response = HTTParty.post('https://push.wombat.co',
+                  body: {
+                      "shipment" => {
+                          "id" => "1283645",
+                          "order_id" => "R154085346",
+                          "email" => "spree@example.com",
+                          "cost" => 5,
+                          "status" => "ready",
+                          "stock_location" => "default",
+                          "shipping_method" => "UPS Ground (USD)",
+                          "tracking" => "12345678",
+                          "shipped_at" => "2014-02-03T17:33:55.343Z",
+                          "channel" => "spree",
+                          "totals" => {
+                              "item" => 200,
+                              "adjustment" => 10,
+                              "tax" => 10,
+                              "shipping" => 0,
+                              "payment" => 210,
+                              "order" => 210
+                          }
+                      }
+                  },
+                  timeout: 240,
+                  headers: { 'Content-Type'   => 'application/json' })
 
     return response if response.code == 200
 
     raise ResponseError, "#{response.code}, API error: #{response.body.inspect}"
   end
 
-  class ResponseError < StandardError; end
+  class ResponseError < StandardError;
+  end
 
 
 end
