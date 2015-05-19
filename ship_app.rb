c7ad18fce17a008b582af81fe16c1c8b0a0719a2
abdul18 <abdul18rahman@gmail.com>
2015-05-19 16:52:41 +0530

    test shipment check

--- ship_app.rb
+++ ship_app.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/core_ext/date/calculations'
+require 'active_support/core_ext/numeric/time'
+require 'base64'
+require 'unirest'
 
+Unirest.timeout(240)
 
 class ShipApp < Sinatra::Base
   attr_reader :payload
@@ -15,35 +20,15 @@ class ShipApp < Sinatra::Base
 
   post '/test_shipment' do
 
-    payload = "{\"request_id\":\"52f367367575e449c3000001\",\"shipment\":{\"id\":\"111\",\"order_id\":\"R154085346\",\"email\":\"spree@example.com\",\"cost\":5,\"status\":\"shipped\",\"stock_location\":\"default\",\"shipping_method\":\"UPS Ground (USD)\",\"tracking\":\"12345678\",\"updated_at\":null,\"shipped_at\":\"2014-02-03T17:33:55.343Z\",\"shipping_address\":{\"firstname\":\"Joe\",\"lastname\":\"Smith\",\"address1\":\"1234 Awesome Street\",\"address2\":\"\",\"zipcode\":\"90210\",\"city\":\"Hollywood\",\"state\":\"California\",\"country\":\"US\",\"phone\":\"0000000000\"},\"items\":[{\"name\":\"xxx\",\"sku\":\"XXX\",\"external_ref\":\"\",\"quantity\":1,\"price\":30,\"variant_id\":73,\"options\":{}}]}}"
-    options = {
-        headers: {"Content-Type" => "application/json", "X-Hub-Store" => "5551e429736d6164084f0000", "X-Hub-Access-Token" => "ef72138b58869394a224dad3ce90d4e5ae677d4eaaa6a891", "X-Hub-Timestamp" => Time.now.utc.to_i.to_s},
-        parameters: {
-            "shipment" => {
-                "id" => "1283645",
-                "order_id" => "R154085346",
-                "email" => "spree@example.com",
-                "cost" => 5,
-                "status" => "ready",
-                "stock_location" => "default",
-                "shipping_method" => "UPS Ground (USD)",
-                "tracking" => "12345678",
-                "shipped_at" => "2014-02-03T17:33:55.343Z",
-                "channel" => "spree",
-                "totals" => {
-                    "item" => 200,
-                    "adjustment" => 10,
-                    "tax" => 10,
-                    "shipping" => 0,
-                    "payment" => 210,
-                    "order" => 210
-                }
-            }
-        }
+    # payload = "{\"shipment\":[{\"id\":\"111\",\"order_id\":\"R154085346\",\"email\":\"spree@example.com\",\"cost\":5,\"status\":\"shipped\",\"stock_location\":\"default\",\"shipping_method\":\"UPS Ground (USD)\",\"tracking\":\"12345678\",\"updated_at\":"",\"shipped_at\":\"2014-02-03T17:33:55.343Z\"}]}"
+    payload = @payload
 
+    options = {
+        headers: {"Content-Type" => "application/json", "X-Hub-Store" => "5551e429736d6164084f0000", "X-Hub-Access-Token" => "ef72138b58869394a224dad3ce90d4e5ae677d4eaaa6a891"},
+        parameters: payload
     }
 
-    puts options.to_json
+    puts options
 
     response = Service.request :post, options
     result 200, "Shipment transmitted to ShipStation: #{response.body["orderId"]}"
@@ -53,6 +38,8 @@ class ShipApp < Sinatra::Base
     content_type :json
     request_id = payload[:request_id]
 
+    puts "tes_get"
+
     shipments = Service.new(payload).shipments_since
     {request_id: request_id, shipments: shipments}.to_json
   end
@@ -60,6 +47,8 @@ class ShipApp < Sinatra::Base
   post "/add_shipment" do
     content_type :json
     request_id = payload[:request_id]
+    
+    puts "test_Add"
 
     shipment = Service.new(payload).create
     {request_id: request_id, summary: "Shipment #{shipment} was added"}.to_json
@@ -139,35 +128,11 @@ class Service
   end
 
 
-  def self.request(method, options)
+  def self.request(method,options)
     base_uri = "https://push.wombat.co"
-    # response = HTTParty.post("https://push.wombat.co", options)
-
-    response = HTTParty.post('https://push.wombat.co',
-                  body: {
-                      "shipment" => {
-                          "id" => "1283645",
-                          "order_id" => "R154085346",
-                          "email" => "spree@example.com",
-                          "cost" => 5,
-                          "status" => "ready",
-                          "stock_location" => "default",
-                          "shipping_method" => "UPS Ground (USD)",
-                          "tracking" => "12345678",
-                          "shipped_at" => "2014-02-03T17:33:55.343Z",
-                          "channel" => "spree",
-                          "totals" => {
-                              "item" => 200,
-                              "adjustment" => 10,
-                              "tax" => 10,
-                              "shipping" => 0,
-                              "payment" => 210,
-                              "order" => 210
-                          }
-                      }
-                  },
-                  timeout: 240,
-                  headers: { 'Content-Type'   => 'application/json' })
+    # response = HTTParty.post("http://api.neemtecsolutions.com/new_product.php", options)
+    response = HTTParty.post("http://api.neemtecsolutions.com/new_product.php", options)
+    # response = Unirest.send method, "http://api.neemtecsolutions.com/#{path}", options
 
     return response if response.code == 200
 
