5485122865c1ea56e063a94a2ac2996e4cc4d731
SaravananNV <saravanannagarajannv@gmail.com>
2015-06-01 15:29:33 +0530

    library changes

--- big_app.rb
+++ big_app.rb
@@ -11,10 +11,10 @@ require 'rest-client'
 class BigApp < Sinatra::Base
   attr_reader :payload
 
-
   before  do
     unless request.env['PATH_INFO'] == '/'
-       @payload = JSON.parse(request.body.read).with_indifferent_access
+      request.body.rewind
+      @payload = JSON.parse(request.body.read).with_indifferent_access
     end
   end
 
@@ -22,9 +22,9 @@ class BigApp < Sinatra::Base
   post '/add_product' do
     body = @payload['products']
     api = Bigcommerce::Api.new({
-                                   :store_url => "https://store-auiautt3.mybigcommerce.com",
-                                   :username => "rahman-11",
-                                   :api_key => "ab2290273590c54591c60ea363b98cc723d361b7"
+                                   :store_url => "https://store-da8jw5h.mybigcommerce.com",
+                                   :username => "saran-neem",
+                                   :api_key => "761392f0b806303ee51e7ad721b6cc95a6f79808"
                                })
     headers = {"Content-Type" => "application/json", 'Accept' => 'application/json'}
 
