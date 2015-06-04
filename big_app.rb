e2f6beadcd418da28824ca803b8e9c95082b4306
abdul18 <abdul18rahman@gmail.com>
2015-06-04 16:36:02 +0530

    i tghink this method correctly for @payload params

--- big_app.rb
+++ big_app.rb
@@ -17,6 +17,8 @@ class BigApp < Sinatra::Base
       request.body.rewind
       @payload = JSON.parse(request.body.read).with_indifferent_access
 
+      puts "#{@payload['parameters']['api_path']}"
+
       @config = Bigcommerce::Api.new({
                                       :store_url => @payload['parameters']['api_path'],
                                       :username => @payload['parameters']['api_username'],
