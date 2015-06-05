2cffc5669728016356abfc052802ede05e13c026
SaravananNV <saravanannagarajannv@gmail.com>
2015-06-05 18:33:44 +0530

    from modified_date last item

--- big_app.rb
+++ big_app.rb
@@ -53,13 +53,13 @@ class BigApp < Sinatra::Base
 
     min_date_modified = @payload['parameters']['min_date_modified']
     product_options = min_date_modified
-      response = Service.request_bigapp :get, "/products", {:max_date_modified => product_options }, @headers, @config1
+      response = Service.request_bigapp :get, "/products", {:min_date_modified => product_options }, @headers, @config1
         final_response = response.to_json
      #  final_response = response.map { |o| Hash[o.each_pair.to_a] }.to_json
      #  puts "#{final_response}"
      #  final_response = final_response.to_json
        puts "response"
-      puts "#{final_response}"
+      puts "#{final_response.last}"
       return final_response
     # end
   end
