809d3a7efbc2249d55324d1f65ac81ee375cf0c8
SaravananNV <saravanannagarajannv@gmail.com>
2015-06-05 13:07:11 +0530

    formt fix

--- big_app.rb
+++ big_app.rb
@@ -53,12 +53,11 @@ class BigApp < Sinatra::Base
     product_options = min_date_modified
       response = Service.request_bigapp :get, "/products", product_options, @headers, @config1
       # final_respone = respone.to_json
-        final_response = JSON.pretty_generate(response.to_json)
-     json_format =  final_response.map { |o| Hash[o.each_pair.to_a] }.to_json
+        final_response = JSON.pretty_generate(response)
      #  final_response = response.map { |o| Hash[o.each_pair.to_a] }.to_json
      #  puts "#{final_response}"
      #  final_response = final_response.to_json
-      return json_format
+      return final_response
      # puts "#{final_response[0]}"
      #  return final_response
     # end
