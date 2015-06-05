d69d58851444570ef5d227f6b1cd4ceea74d6d03
SaravananNV <saravanannagarajannv@gmail.com>
2015-06-05 14:29:35 +0530

    get json print output

--- big_app.rb
+++ big_app.rb
@@ -53,10 +53,12 @@ class BigApp < Sinatra::Base
     product_options = min_date_modified
       response = Service.request_bigapp :get, "/products", product_options, @headers, @config1
       # final_respone = respone.to_json
-        final_response = JSON.pretty_generate(response)
+        final_response = response.to_json
      #  final_response = response.map { |o| Hash[o.each_pair.to_a] }.to_json
      #  puts "#{final_response}"
      #  final_response = final_response.to_json
+       puts "response"
+      puts "#{final_response}"
       return final_response
      # puts "#{final_response[0]}"
      #  return final_response
