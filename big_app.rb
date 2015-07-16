e4f459032a489b0430b80e68562a1d1a0d5fc215
abdul18 <abdul18rahman@gmail.com>
2015-07-16 12:22:47 +0530

    console check payload value

--- big_app.rb
+++ big_app.rb
@@ -72,6 +72,8 @@ class BigApp < Sinatra::Base
   end
 
   post '/get_customers' do
+    logger.info("----------------------------------------------------------------------")
+    puts("get_order test")
     content_type :json
     get_customer_data =  @payload['parameters']['min_date_created']
     customer_options = get_customer_data
@@ -91,7 +93,7 @@ class BigApp < Sinatra::Base
 
   post '/get_orders' do
     logger.info("----------------------------------------------------------------------")
-    logger.info("get_order test")
+    puts("get_order test")
     content_type :json
     min_date_modified =  @payload['parameters']['min_date_modified']
     order_options = min_date_modified
@@ -107,6 +109,8 @@ class BigApp < Sinatra::Base
   end
 
   post '/get_shipments' do
+    logger.info("----------------------------------------------------------------------")
+    puts("get_order test")
     content_type :json
     shipment_datas = []
     list_orders = Service.list_all_order(@config1,@headers)
