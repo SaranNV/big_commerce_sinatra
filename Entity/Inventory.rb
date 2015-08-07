b80fce2cb7dedf30a8136e47cab0e44d608c3ead
abdul18 <abdul18rahman@gmail.com>
2015-08-07 11:10:29 +0530

    set inventory change update

--- Entity/Inventory.rb
+++ Entity/Inventory.rb
@@ -10,25 +10,25 @@ module Entity
       if !get_product.empty?
           product_id = get_product.first['id']
           get_inventory_track =  Service.request_bigapp :get,"/products/#{product_id}","", headers, config
-          if !get_inventory_track['inventory_tracking'] == 'none'
+          if get_inventory_track['inventory_tracking'] == 'none'
+            response =  {:error => "This product does not have inventory tracking enabled."}
+            return JSON.pretty_generate(response)
+          else
             response = Service.request_bigapp :put,"/products/#{product_id}",bigcommerce_data, headers, config
             response_message = {:message => "The Inventory #{wombat_object['id']} for product: #{wombat_object['product_id']} was updated in BigCommerce"}
             return JSON.pretty_generate(response_message)
-          else
-            response =  {:error => "This product does not have inventory tracking enabled."}
-            return JSON.pretty_generate(response)
           end
       elsif !find_skus_product.empty?
           product_id = find_skus_product.first['product_id']
           sku_id = find_skus_product.first['id']
           get_inventory_track =  Service.request_bigapp :get,"/products/#{product_id}","", headers, config
-          if !get_inventory_track['inventory_tracking'] == 'none'
+          if get_inventory_track['inventory_tracking'] == 'none'
+            response =  {:error => "This product does not have inventory tracking enabled."}
+            return JSON.pretty_generate(response)
+          else
             response = Service.request_bigapp :put,"/products/#{product_id}/skus/#{sku_id}",bigcommerce_data, headers, config
             response_message = {:message => "The Inventory #{wombat_object['id']} for product: #{wombat_object['product_id']} was updated in BigCommerce"}
             return JSON.pretty_generate(response_message)
-          else
-            response =  {:error => "This product does not have inventory tracking enabled."}
-            return JSON.pretty_generate(response)
           end
       else
         response = {:message => "No product could be found for sku"}
